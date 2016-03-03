f196462bb2dd5f4363834e9b24f48248aaf4db61
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2016-03-03 11:28:35 +0200

    added table printout

--- table.php
+++ table.php
@@ -8,7 +8,7 @@
 	$mysql = new mysqli("localhost", $db_username, $db_password, "webpr2016_romil");
 	
 	//SQL sentence
-	$stmt = $mysql->prepare("SELECT id, recipient, message, created FROM messages_sample");
+	$stmt = $mysql->prepare("SELECT id, recipient, message, created FROM messages_sample ORDER BY created DESC LIMIT 10");
 	
 	//if error in sentence
 	echo $mysql->error;
@@ -19,14 +19,33 @@
 	//query
 	$stmt->execute();
 	
+	$table_html = "";
+	
+	//add smth to string .=
+	$table_html .= "<table>";
+		$table_html .= "<tr>";
+			$table_html .= "<th>ID</th>";
+			$table_html .= "<th>Recipient</th>";
+			$table_html .= "<th>Message</th>";
+			$table_html .= "<th>Created</th>";
+		$table_html .= "</tr>";
+	
 	// GET RESULT 
 	//we have multiple rows
 	while($stmt->fetch()){
 		
 		//DO SOMETHING FOR EACH ROW
-		echo $id." ".$message."<br>";
-		
+		//echo $id." ".$message."<br>";
+		$table_html .= "<tr>"; //start new row
+			$table_html .= "<td>".$id."</td>"; //add columns
+			$table_html .= "<td>".$recipient."</td>";
+			$table_html .= "<td>".$message."</td>";
+			$table_html .= "<td>".$created."</td>";
+		$table_html .= "</tr>"; //end row
+
 	}
+	$table_html .= "</table>";
+	echo $table_html;
 	
 	
 	
